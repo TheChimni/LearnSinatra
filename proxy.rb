bb6783297c87ab71cb3cb28151ca1267df825fea
Rieethaa <rieethaa@gmail.com>
2012-09-25 20:42:02 +0100

    Building the proxy Sinatra app

--- proxy.rb
+++ proxy.rb
@@ -1,5 +1,12 @@
 # encoding: utf-8
 
-#
-# Oh no! I lost my codez :'(
-#
+require 'rubygems'
+require 'sinatra'
+
+configure do
+  set :port, 4568
+end
+
+get '/' do
+  puts 'Welcome to Proxy!'
+end
