018afc400f2d3f8a8187abe388e659c68af6e4b9
Rieethaa <rieethaa@gmail.com>
2012-09-26 20:13:18 +0100

    Re-factored the code. Removed duplication

--- proxy.rb
+++ proxy.rb
@@ -9,24 +9,14 @@ configure do
    set :port => 4568
 end
 
-get '/' do
-  uri =  URI.parse("http://localhost:4567")
+get '/*' do
+  path = params[:splat].first
+  uri = URI.parse("http://localhost:4567/#{path}")
 
   # Forward the client request to server after adding 'x-api-key' header 
   http = Net::HTTP.new(uri.host, uri.port)
   request = Net::HTTP::Get.new(uri.request_uri)
   request['X-API-Key'] = 'awesomeserver'
-  response = http.request(request)
-  response.body
-end
-
-get '/hello_world' do
-  uri =  URI.parse("http://localhost:4567/hello_world?api_key=awesomeserver")
-
-  # Forward the client request to server after adding the api_key param
-  http = Net::HTTP.new(uri.host, uri.port)
-  request = Net::HTTP::Get.new(uri.request_uri)
-  # request['X-API-Key'] = 'awesomeserver'
   response = http.request(request)
   response.body
 end
